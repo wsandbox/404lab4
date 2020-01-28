efa2f8004ee170ac954a39ec32a62a407b3af84d
wsandbox <warrenssandbox@gmail.com>
2020-01-27 23:29:05 -0500

    adding lab files

--- polls/views.py
+++ polls/views.py
@@ -26,7 +26,6 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
